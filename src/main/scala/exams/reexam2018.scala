5419f704198031629b7879ea24a860704ffb0dcc
Jeppeks1 <Jeppeks1@hotmail.com>
2018-08-22 23:08:09 +0200

    Created a template file for the re-exam

---
+++ src/main/scala/exams/reexam2018.scala
@@ -0,0 +1,37 @@
+package exams
+
+import scala.language.higherKinds
+
+import handIns.stream.Exercise4
+import handIns.stream.Exercise4._
+import handIns.stream.Exercise4.Stream
+import handIns.stream.Exercise4.Stream._
+
+import handIns.fpinscala.monoids.Monoid
+import handIns.fpinscala.monads.Monad
+import handIns.fpinscala.monads.Functor
+
+import handIns.functionalState.State
+import handIns.functionalState.State._
+import handIns.functionalState.RNG
+
+import handIns.parallelism.Par.Par
+import handIns.parallelism.Par._
+
+import handIns.fingerTree.FingerTree
+import handIns.generator.Gen
+import monocle.Optional
+import monocle.Lens
+
+object reexam2018 {
+
+  object Q1 {
+
+  }
+
+
+  object Q2 {
+
+  }
+
+}
